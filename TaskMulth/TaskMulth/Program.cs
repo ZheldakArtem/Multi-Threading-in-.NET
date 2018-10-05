780f4f9fa4d007a428cc465a8ce050b7c2e1b0dc
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-05 18:44:32 +0300

    Finishing two tasks

---
+++ TaskMulth/TaskMulth/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskMulth
+{
+	class Program
+	{
+		private const int N = 100;
+		private const int Iterations = 1000;
+		static void Main(string[] args)
+		{
+			Task[] taskArr = CreateTasks(N);
+
+			foreach (var task in taskArr)
+			{
+				task.Start();
+			}
+
+			Task.WaitAll(taskArr);
+		}
+
+		private static Task[] CreateTasks(int n)
+		{
+			Task[] tasks = new Task[n];
+			return Enumerable.Range(0, n).Select(i => new Task(Work, i)).ToArray();
+		}
+
+		private static void Work(object numberOfTask)
+		{
+			for (int i = 0; i < Iterations; i++)
+			{
+				Console.WriteLine($"Task #{numberOfTask} - {i}");
+			}
+		}
+	}
+}
---
+++ TaskMulth/TaskMulth2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMulth2
+{
+	class Program
+	{
+
+		private const int N = 10;
+		static void Main(string[] args)
+		{
+			var task = new Task<int[]>(CreateIntArray);
+
+		task.ContinueWith(tArray => MultiplieArr(tArray.Result))
+				.ContinueWith(tArray => SortByAscending(tArray.Result))
+					.ContinueWith(tArray => ColculateAverage(tArray.Result));
+
+			task.Start();
+
+			task.Wait(); // Поток ждет выполнения только первой задачи(т.е. выводится результат первой задачи, затем вывод 'End' и потом вывод всех остальных
+
+			Console.WriteLine("End");
+
+			Console.ReadLine();
+		}
+
+		static int[] CreateIntArray()
+		{
+			Console.WriteLine("------------SUMM----------------");
+			int[] arr = new int[N];
+			var random = new Random();
+			for (int i = 0; i < N; i++)
+			{
+				arr[i] = random.Next(100);
+				Console.WriteLine(arr[i]);
+			}
+
+			return arr;
+		}
+
+		static int[] MultiplieArr(int[] array)
+		{
+			Console.WriteLine("------------Multiplie--------------");
+			Random random = new Random();
+			int randomNumber;
+			int temp;
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				temp = array[i];
+				randomNumber = random.Next(100);
+				array[i] *= randomNumber;
+
+				Console.WriteLine($"{temp} * {randomNumber} = {array[i]}");
+			}
+
+			return array;
+		}
+
+		static int[] SortByAscending(int[] array)
+		{
+			Console.WriteLine("-----------SortByAscending-------------");
+			Array.Sort(array);
+			foreach (var item in array)
+			{
+				Console.WriteLine(item);
+			}
+
+			return array;
+		}
+
+		private static void ColculateAverage(int[] array)
+		{
+			Console.WriteLine("------------Average-------------");
+			double average = array.Average();
+			Console.WriteLine(average);
+		}
+	}
+}
