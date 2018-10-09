682f4a8ef96cd66931378256397d972d0eb2c57c
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-09 18:23:34 +0300

    Added TaskAsync3

---
+++ TaskAsync/TaskAsync2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAsync2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string command = string.Empty;
+			Task<byte[]>[] taskArr;
+			WebClient[] wc;
+
+			do
+			{
+				Console.WriteLine("Enter one or more address. Use ';' as a separator");
+				string temp = "https://www.tut.by/;https://www.onliner.by/";
+				string[] urls = temp.Split(';');//Console.ReadLine().Split(';')   uncomment to write url manually
+
+				wc = new WebClient[urls.Length];
+				taskArr = new Task<byte[]>[urls.Length];
+
+				for (int i = 0; i < urls.Length; i++)
+				{
+					wc[i] = new WebClient();
+					taskArr[i] = DownloadDataAsync(urls[i], wc[i]);
+				}
+
+				Console.WriteLine("1. Enter '1' to exit;");
+				Console.WriteLine("2. Enter '2' to prevent loading and try again;");
+				Console.WriteLine("3. Enter '3' to get result;");
+
+				command = Console.ReadLine();
+
+				switch (command)
+				{
+					case "1":
+					case "3":
+						break;
+					case "2":
+						foreach (var wClient in wc)
+						{
+							wClient.CancelAsync();
+						}
+						continue;
+				}
+
+			} while (command == "2");
+
+			if (command == "3")
+			{
+				Task.WaitAll(taskArr);
+				foreach (var task in taskArr)
+				{
+					var result = Encoding.UTF8.GetString(task.Result);
+					Console.WriteLine(result);
+				}
+			}
+
+			Console.ReadLine();
+		}
+
+		public static async Task<byte[]> DownloadDataAsync(string url, WebClient wClient)
+		{
+			byte[] result = await wClient.DownloadDataTaskAsync(url);
+
+			return result;
+
+		}
+	}
+
+}
--- TaskAsync/TaskAsync3/Program.cs
+++ TaskAsync/TaskAsync3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskAsync3
@@ -10,6 +11,77 @@ namespace TaskAsync3
 	{
 		static void Main(string[] args)
 		{
+			var backet = new List<Product>();
+
+			var products = CreateListOfProducts();
+			int productId;
+
+			do
+			{
+				ShowProducts(products);
+
+				Console.WriteLine(" Choose good and press 'Enter'.");
+
+				if (int.TryParse(Console.ReadLine(), out productId))
+				{
+					backet.Add(products.FirstOrDefault(p => p.Id == productId));
+
+					var task = RecountPriceAndShowAsync(backet);
+				}
+				else
+				{
+					Console.WriteLine("Bad product ID");
+				}
+				
+				Console.WriteLine("Enter 'ex' to exit");
+ 
+			} while (string.Compare(Console.ReadLine(), "ex", true) != 0);
+
+			Console.ReadLine();
+
 		}
+
+		private static void ShowAllPrice(IList<Product> backet)
+		{
+			//Thread.Sleep(5000); // work
+			decimal resultPrice = 0;
+			foreach (var item in backet)
+			{
+				resultPrice += item.Price;
+			}
+			Console.WriteLine($"ALL PRICE: {resultPrice}");
+		}
+
+		private static void ShowProducts(IList<Product> products)
+		{
+			Console.WriteLine("ID \t| Product Name \t| Price ");
+			Console.WriteLine("---------------------------------");
+			foreach (var item in products)
+			{
+				Console.WriteLine($"{item.Id} \t| {item.Title} \t| {item.Price}\t");
+			}
+			Console.WriteLine("---------------------------------");
+		}
+
+		public static IList<Product> CreateListOfProducts()
+		{
+			return new List<Product>()
+			{
+				new Product() {Id = 1, Price = 100.9m, Title = "Bed\t"},
+				new Product() {Id = 2, Price = 300m, Title = "TV\t"},
+				new Product() {Id = 3, Price = 12500.4m, Title = "Car\t"},
+				new Product() {Id = 4, Price = 379.9m, Title = "Bicycle"},
+				new Product() {Id = 5, Price = 200m, Title = "Microwave"},
+				new Product() {Id = 6, Price = 490.2m, Title = "Phone"},
+				new Product() {Id = 7, Price = 700m, Title = "Fridge"},
+			};
+		}
+
+		public static async Task RecountPriceAndShowAsync(IList<Product> backet)
+		{
+			await Task.Run(() => ShowAllPrice(backet));
+
+		}
+
 	}
 }
