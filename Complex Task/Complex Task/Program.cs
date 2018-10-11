2c86a1e1e7fc970862d7250e7072871dbb54e7d7
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-11 16:03:49 +0300

    Complex task added

---
+++ Complex Task/Complex Task/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Complex_Task
+{
+	class Program
+	{
+		private static int port = 8005;
+		private static ConcurrentBag<string> historyOfMessages = new ConcurrentBag<string>();
+		private static int N = 10;
+
+		static void Main(string[] args)
+		{
+
+			IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+			Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			List<Socket> clienSocketList = new List<Socket>(); ;
+			try
+			{
+
+				listenSocket.Bind(ipPoint);
+
+				listenSocket.Listen(10);
+
+				Console.WriteLine("Server waiting for connection...");
+				while (true)
+				{
+					Socket handler = listenSocket.Accept();
+					clienSocketList.Add(handler);
+
+					if (historyOfMessages.Count > 0)
+					{
+						var lastTen = historyOfMessages.Count > N ? historyOfMessages.Skip(historyOfMessages.Count - N) : historyOfMessages;//historyOfMessages.Skip();
+
+						handler.Send(Encoding.Unicode.GetBytes("History:\n"));
+						foreach (var messageItem in lastTen)
+						{
+							handler.Send(Encoding.Unicode.GetBytes(messageItem + '\n'));
+						}
+					}
+
+					ThreadPool.QueueUserWorkItem((state) =>
+					{
+						StringBuilder builder = new StringBuilder();
+						int bytes = 0;
+						byte[] data = new byte[256];
+						Socket selfSocket = handler;
+
+						while (true)
+						{
+							try
+							{
+								if (selfSocket.Available > 0)
+								{
+									do
+									{
+										bytes = selfSocket.Receive(data);
+										builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+									} while (selfSocket.Available > 0);
+
+									var temp = builder.ToString();
+									if (builder.ToString() == "Disconnect")
+									{
+										clienSocketList.Remove(selfSocket);
+										selfSocket.Shutdown(SocketShutdown.Both);
+										selfSocket.Close();
+										break;
+									}
+									historyOfMessages.Add(builder.ToString());
+
+									Console.WriteLine("----------------------");
+									Console.WriteLine(builder.ToString());
+									Console.WriteLine("----------------------");
+
+									foreach (var client in clienSocketList)
+									{
+										if (selfSocket != client)
+										{
+											client.Send(Encoding.Unicode.GetBytes(builder.ToString()));
+										}
+									}
+									builder.Clear();
+								}
+							}
+							catch (Exception)
+							{
+								break;
+							}
+						}
+					});
+				}
+			}
+			catch (Exception)
+			{
+				foreach (var socket in clienSocketList)
+				{
+					socket.Shutdown(SocketShutdown.Both);
+					socket.Close();
+				}
+				listenSocket.Close();
+			}
+		}
+	}
+}
---
+++ Complex Task/Complex_Task_Client/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Complex_Task_Client
+{
+	class Program
+	{
+		// адрес и порт сервера, к которому будем подключаться
+		static int port = 8005; // порт сервера
+		static string address = "127.0.0.1"; // адрес сервера
+
+		static List<string> messages = new List<string>()
+		{
+			"Message - 1;", "Message - 2;",
+			"Message - 3;", "Message - 4;",
+			"Message - 5;", "Message - 6;",
+			"Message - 7;", "Message - 8;",
+			"Message - 9;", "Message - 10;"
+		};
+
+		static void Main(string[] args)
+		{
+			IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+			string command = string.Empty;
+			Random random = new Random();
+			// Create the token source.
+			CancellationTokenSource cts;
+			Socket socket;
+
+			do
+			{
+				Thread.Sleep(5000); //small delay to server remove deleted connection
+				cts = new CancellationTokenSource();
+				socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.Connect(ipPoint);
+
+				try
+				{
+					// send message to the server
+					ThreadPool.QueueUserWorkItem((obj) =>
+					{
+						string message = string.Empty;
+						byte[] dataSend;
+						int messageNumber;
+						CancellationToken token = (CancellationToken)obj;
+
+						for (int i = 0; i < random.Next(1, 10); i++)
+						{
+							Thread.Sleep(random.Next(1000, 3000));
+							if (token.IsCancellationRequested)
+							{
+								break;
+							}
+
+							messageNumber = random.Next(0, messages.Count);
+							message = messages[messageNumber];
+							dataSend = Encoding.Unicode.GetBytes(message);
+							socket.Send(dataSend);
+						}
+					}, cts.Token);
+
+					// retrieve data from server and print to the console
+					ThreadPool.QueueUserWorkItem((obj) =>
+					{
+						byte[] data = new byte[256];
+						StringBuilder builder = new StringBuilder();
+						int bytes = 0;
+						CancellationToken token = (CancellationToken)obj;
+
+						Console.WriteLine("Waiting messages from other clients");
+						while (true)
+						{
+							try
+							{
+								if (token.IsCancellationRequested)
+								{
+									socket.Send(Encoding.Unicode.GetBytes("Disconnect"));
+									socket.Shutdown(SocketShutdown.Both);
+									socket.Close();
+									break;
+								}
+								if (socket.Available > 0)
+								{
+									do
+									{
+										bytes = socket.Receive(data, data.Length, 0);
+										builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+									}
+									while (socket.Available > 0);
+									if (builder.Length > 0)
+									{
+										Console.WriteLine("Responce from the server:\n" + builder.ToString());
+									}
+									builder.Clear();
+								}
+							}
+							catch (Exception)
+							{
+								break;
+							}
+						}
+					}, cts.Token);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+
+				Console.WriteLine("Close client? Y/N");
+
+				command = Console.ReadLine();
+
+				if (command == "N")
+				{
+					cts.Cancel();
+					cts.Dispose();
+				}
+
+			} while (string.Compare(command, "Y", true) != 0);
+
+			socket.Shutdown(SocketShutdown.Both);
+			socket.Close();
+
+			Console.Read();
+		}
+	}
+}
